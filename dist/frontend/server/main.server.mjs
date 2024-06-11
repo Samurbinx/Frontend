3b5ac177a4fa2135051179af60a30e9a37464ba1
samurbinx <samurbinx@gmail.com>
2024-06-11 17:49:31 +0200

    modified:   dist/frontend/browser/contacto/index.html 	modified:   dist/frontend/browser/home/index.html 	modified:   dist/frontend/browser/index.html 	modified:   dist/frontend/browser/login/index.html 	renamed:    dist/frontend/browser/main-YE47URI3.js -> dist/frontend/browser/main-DPKTPIIC.js 	deleted:    dist/frontend/browser/media/Autorretrato-XPMABXGA.png 	new file:   dist/frontend/browser/media/IMG_1869-ILVKYYVK.JPG 	modified:   dist/frontend/browser/proyectos/index.html 	modified:   dist/frontend/browser/registro/index.html 	modified:   dist/frontend/browser/sobremi/index.html 	modified:   dist/frontend/browser/tienda/index.html 	renamed:    dist/frontend/server/chunk-FLUL7PVF.mjs -> dist/frontend/server/chunk-3J77VF5M.mjs 	modified:   dist/frontend/server/index.server.html 	modified:   dist/frontend/server/main.server.mjs 	modified:   dist/frontend/server/server.mjs 	modified:   src/app/components/contact/contact.component.css 	modified:   src/app/components/login/login.component.css 	modified:   src/app/components/register/register.component.css 	modified:   src/app/components/shop/shop.component.css 	modified:   src/app/components/workdetail/workdetail.component.css 	modified:   src/app/components/workdetail/workdetail.component.html 	modified:   src/styles.css

--- dist/frontend/server/main.server.mjs
+++ dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-FLUL7PVF.mjs";import"./chunk-RPRBIDQ5.mjs";import"./chunk-Z2LFJOBV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-3J77VF5M.mjs";import"./chunk-RPRBIDQ5.mjs";import"./chunk-Z2LFJOBV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};

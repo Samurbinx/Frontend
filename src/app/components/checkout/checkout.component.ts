325124ab8a1c2311e4ac9df81fbf2431f5d674db
samurbinx <samurbinx@gmail.com>
2024-11-16 18:34:27 +0100

    modified:   angular.json 	modified:   src/app/components/checkout/checkout.component.css 	modified:   src/app/components/checkout/checkout.component.html 	modified:   src/app/components/checkout/checkout.component.ts 	modified:   src/main.ts

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,3 +1,5 @@
+import { User } from './../../interfaces/user.interface';
+import { AuthService } from './../../services/auth.service';
 import { CartService } from '../../services/cart.service';
 import { Component, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { ReactiveFormsModule, UntypedFormBuilder, Validators } from '@angular/forms';
@@ -6,31 +8,53 @@ import { MatInputModule } from '@angular/material/input';
 
 import {
   injectStripe,
+  NGX_STRIPE_VERSION,
+  STRIPE_PUBLISHABLE_KEY,
+  STRIPE_OPTIONS,
+  NgxStripeModule,
   StripeElementsDirective,
-  StripePaymentElementComponent
+  StripePaymentElementComponent,
+  StripeFactoryService,
+  LazyStripeAPILoader,
+  WindowRef,
+  DocumentRef
 } from 'ngx-stripe';
 import {
   StripeElementsOptions,
   StripePaymentElementOptions
 } from '@stripe/stripe-js';
-
+import { UserModel } from '../../models/user.model';
 @Component({
   selector: 'ngstr-checkout-form',
   templateUrl: './checkout.component.html',
+  styleUrl: './checkout.component.css',
   standalone: true,
   imports: [
     ReactiveFormsModule,
     MatInputModule,
     StripeElementsDirective,
-    StripePaymentElementComponent
-  ]
+    StripePaymentElementComponent,
+    NgxStripeModule,
+  ],
+  providers: [StripeFactoryService,
+    { provide: NGX_STRIPE_VERSION, useValue: '14.1.1' },
+    { provide: STRIPE_PUBLISHABLE_KEY, useValue: 'pk_test_51QL60A01qslkTUypDH7HjcBn7G0E22306bHTsSjDqsGNsK3LT04ipA6PeGp4IajYdwNcIqce2Fi8hgHf4oFCtfMA006sUUYNnq' },
+    { provide: STRIPE_OPTIONS, useValue: { apiVersion: '2020-08-27' } },
+    { provide: LazyStripeAPILoader, useClass: LazyStripeAPILoader }, 
+    { provide: WindowRef, useClass: WindowRef },  
+    { provide: DocumentRef, useClass: DocumentRef }, 
+  ],
 })
 export class CheckoutComponent implements OnInit {
   @ViewChild(StripePaymentElementComponent)
   paymentElement!: StripePaymentElementComponent;
 
+  user: UserModel | null = null;
+  // user: UserModel | null = new UserModel('samurbinx@gmail.com', 'samurbinx', 'Samuel', 'Urbina Flor', 'Surbinx', '622039286');
+
   private readonly fb = inject(UntypedFormBuilder);
   private readonly cartService = inject(CartService);
+  private readonly authService = inject(AuthService);
 
   paymentElementForm = this.fb.group({
     name: ['', [Validators.required]],
@@ -61,8 +85,6 @@ export class CheckoutComponent implements OnInit {
   paying = signal(false);
 
   ngOnInit() {
-    // const amount = this.paymentElementForm.get('amount')?.value || 1000;
-
     this.cartService.totalAmount$.subscribe((total) => {
       const amount = total;
       if (amount) {
@@ -74,7 +96,10 @@ export class CheckoutComponent implements OnInit {
       }
     });
 
-
+    // let userid = this.authService.getUserId();
+    // if (userid) {
+    //   this.user = this.authService.getUser();
+    // }
   }
 
   pay() {
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ import { provideNgxStripe } from 'ngx-stripe';
 bootstrapApplication(AppComponent, {
   providers: [
     ...appConfig.providers,  // Si tienes más proveedores en appConfig, los mantienes
-    provideNgxStripe('pk_test_51QL60A01qslkTUypDH7HjcBn7G0E22306bHTsSjDqsGNsK3LT04ipA6PeGp4IajYdwNcIqce2Fi8hgHf4oFCtfMA006sUUYNnq')  // Aquí agregas el proveedor de Stripe
+    provideNgxStripe('pk_test_51QL60A01qslkTUypDH7HjcBn7G0E22306bHTsSjDqsGNsK3LT04ipA6PeGp4IajYdwNcIqce2Fi8hgHf4oFCtfMA006sUUYNnq')
   ]
 }).catch((err) => console.error(err));

db06d341a7a898fe7ece2febb7e75b07df380a34
samurbinx <samurbinx@gmail.com>
2024-12-05 16:44:43 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   dist/frontend/browser/carrito/index.html
    	modified:   dist/frontend/browser/checkout/index.html
    	modified:   dist/frontend/browser/contacto/index.html
    	modified:   dist/frontend/browser/home/index.html
    	modified:   dist/frontend/browser/index.csr.html
    	modified:   dist/frontend/browser/login/index.html
    	deleted:    dist/frontend/browser/main-BCKKIUOM.js
    	new file:   dist/frontend/browser/main-U7FFPYOH.js
    	modified:   dist/frontend/browser/proyecto/index.html
    	modified:   dist/frontend/browser/proyectos/index.html
    	modified:   dist/frontend/browser/registro/index.html
    	modified:   dist/frontend/browser/sobremi/index.html
    	modified:   dist/frontend/browser/userdata/favoritos/index.html
    	modified:   dist/frontend/browser/userdata/index.html
    	modified:   dist/frontend/browser/userdata/mydata/address/index.html
    	modified:   dist/frontend/browser/userdata/mydata/data/index.html
    	modified:   dist/frontend/browser/userdata/mydata/index.html
    	modified:   dist/frontend/browser/userdata/mydata/menu/index.html
    	modified:   dist/frontend/browser/userdata/mydata/pwd/index.html
    	modified:   dist/frontend/browser/userdata/pedidos/index.html
    	new file:   dist/frontend/server/chunk-MH4BMBAZ.mjs
    	deleted:    dist/frontend/server/chunk-XMHVLGQ7.mjs
    	modified:   dist/frontend/server/index.server.html
    	modified:   dist/frontend/server/main.server.mjs
    	modified:   dist/frontend/server/server.mjs

--- dist/frontend/server/main.server.mjs
+++ dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XMHVLGQ7.mjs";import"./chunk-23N436UT.mjs";import"./chunk-XXLCMA4D.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-MH4BMBAZ.mjs";import"./chunk-23N436UT.mjs";import"./chunk-XXLCMA4D.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};

c22ace17384cd9ede9af26b055b47110695c839e
samurbinx <samurbinx@gmail.com>
2024-12-08 18:19:15 +0100

    modified:   src/environments/environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
    production: true,
   //  apiUrl: 'http://localhost:8080',
    apiUrl: 'http://192.168.1.135:8080',
-   apiUrlFront: 'http://192.168.1.135:4200'
+   apiUrlFront: 'http://192.168.1.135'
  };
  
